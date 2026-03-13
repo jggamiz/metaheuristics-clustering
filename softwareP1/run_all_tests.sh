41f737ad7de2f54a8b5100a47be59476ca79d9b2
Jorge García Gámiz <129839114+jggamiz@users.noreply.github.com>
2026-03-13 21:20:01 +0100

    Add files via upload

---
+++ softwareP1/run_all_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Starting the complete test battery..."
+
+# BUPA
+./bin/par_solver data/bupa_set 15 all
+./bin/par_solver data/bupa_set 30 all
+
+# GLASS
+./bin/par_solver data/glass_set 15 all
+./bin/par_solver data/glass_set 30 all
+
+# ZOO
+./bin/par_solver data/zoo_set 15 all
+./bin/par_solver data/zoo_set 30 all
+
+echo "All tests finished! CSV files generated."
